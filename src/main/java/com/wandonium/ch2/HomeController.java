3da3e2d783332d2659647e3c54a66dc757739eae
Wandonium <hillarywando@gmail.com>
2023-04-06 16:40:40 +0300

    Added a PostMapping to the app.
    Added a PostMapping to the controller as well as the corresponding html form to the UI template.

--- src/main/java/com/wandonium/ch2/HomeController.java
+++ src/main/java/com/wandonium/ch2/HomeController.java
@@ -3,6 +3,8 @@ package com.wandonium.ch2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -19,4 +21,9 @@ public class HomeController {
         return "index";
     }
     
+    @PostMapping("/new-video")
+    public String newVideo(@ModelAttribute Video newVideo) {
+        videoService.create(newVideo);
+        return "redirect:/";
+    }
 }
